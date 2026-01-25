c6fd46f9cbddf7fbf939d4502180db918d9b5e5a
ishwork <ishworkhadka70@gmail.com>
2026-01-25 22:24:43 +0200

    refactor: update imports to follow React router v7 convention

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router';
 
 import Home from './pages/Home';
 import Users from './pages/Users';
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 import { useActionState } from 'react';
 import { createUserAction } from '../actions/userActions';
 
